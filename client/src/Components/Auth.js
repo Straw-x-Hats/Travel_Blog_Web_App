4a30fe4a727dd1072d4d9c4f3c3cdd51fe25bcfb
Yethu Kichu <123456789+odin@users.noreply.github.com>
2022-11-04 22:12:05 +0530

    updated

--- client/src/Components/Auth.js
+++ client/src/Components/Auth.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Button, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import './style.css'
+import { useState } from 'react'
 function Auth() {
+  const [isSignUp, setisSignUp] = useState(false)
   return (
     <div>
       <div className='main'>
@@ -11,13 +13,13 @@ function Auth() {
         <Typography  sx={{
           fontSize:30,
           fontWeight:900
-  }}>Login</Typography>
+  }}>{isSignUp? "Login":"SignUp"}</Typography>
     
-       <TextField name='name'   placeholder='name' sx={{width:400}}/>
+    { !isSignUp &&    <TextField name='name'   placeholder='name' sx={{width:400}}/>}
         <TextField name='email'   type={'email'} placeholder='email' sx={{width:400}}/>
         <TextField name='password'   type={'password'} placeholder='password' sx={{width:400}}/>
         <Button type='submit' variant='contained' color='warning'>Submit</Button>
-        <Button type='submit' variant='outlined' >Change to SignUP</Button>
+        <Button  variant='outlined' >Change to SignUP</Button>
        
       </Box>
       </form>
