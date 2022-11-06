76f13c9450cf378d82c01ad27d10582bd2bb65f7
Yethu Kichu <123456789+odin@users.noreply.github.com>
2022-11-06 12:39:05 +0530

    updated

--- client/src/Components/Auth.js
+++ client/src/Components/Auth.js
@@ -1,23 +1,59 @@
 import React from 'react'
-import { Button, TextField, Typography } from '@mui/material'
+import { Button, Input, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import './style.css'
+import axios from 'axios'
 import { useState } from 'react'
+
 function Auth() {
   const [isSignUp, setisSignUp] = useState(false)
+  const [input, setinput] = useState({
+    name:'',
+    email:'',
+    password:''
+  })
+  const handleChange=(e)=>{
+    setinput(prev=>({
+     ...prev,[e.target.name]:[e.target.value]
+    
+    })) 
+ }
+ const handleSubmit =(e)=>{
+  e.preventDefault()
+  if(!isSignUp){
+    sendRequest("signup").then((data)=>console.log(data))
+    
+    .catch((e)=>console.log(e))
+  }
+  else{
+    sendRequest().then((data)=>console.log(data))
+    .catch((e)=>console.log(e))
+  }
+ 
+ }
+  const sendRequest=async(type="login")=>{
+  const res = await axios.post(`http://localhost:5000/user/${type}`,{
+    name:input.name ? input.name:'',
+    email:String(input.email),
+    password:String(input.password),
+   }).catch((e)=>console.log(e))
+   const data = res.data
+   
+   return data 
+  }
   return (
     <div>
       <div className='main'>
-      <form >
+      <form onSubmit={handleSubmit}>
         <Box className='form' bgcolor='inherit'>
         <Typography  sx={{
           fontSize:30,
           fontWeight:900
   }}>{isSignUp? "Login":"SignUp"}</Typography>
     
-    { !isSignUp &&    <TextField name='name'   placeholder='name' sx={{width:400}}/>}
-        <TextField name='email'   type={'email'} placeholder='email' sx={{width:400}}/>
-        <TextField name='password'   type={'password'} placeholder='password' sx={{width:400}}/>
+    { !isSignUp &&    <TextField onChange={handleChange} name='name' value={input.name}   placeholder='name' sx={{width:400}}/>}
+        <TextField onChange={handleChange} name='email' value={input.email}   type={'email'} placeholder='email' sx={{width:400}}/>
+        <TextField onChange={handleChange} name='password' value={input.password}   type={'password'} placeholder='password' sx={{width:400}}/>
         <Button type='submit' variant='contained' color='warning'>Submit</Button>
         <Button  variant='outlined' onClick={()=>{setisSignUp(!isSignUp)}} >Change to {isSignUp? "SignUp":"Login"}</Button>
        
